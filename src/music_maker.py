46a75a5bcce6aeb5be3974a6695b62c2cc43c3d8
Georgie C <g.l.challis@student.reading.ac.uk>
2020-07-16 23:52:49 +0100

    docs:  Update file name

--- src/music_maker.py
+++ src/music_maker.py
@@ -12,7 +12,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Joy detection demo."""
+
+"""Georgie's Music Maker demo."""
+
 import argparse
 import collections
 import contextlib
